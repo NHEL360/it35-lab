7d038f73baafc66349f9ea9e650a02e7bbc90a42
NHEL360 <nhelmeynard@gmail.com>
2025-03-06 13:44:13 +0800

    adding design on button on about.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,28 +1,51 @@
+import React from 'react';
 import { 
     IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar 
-  } from '@ionic/react';
-  
-  const About: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>About</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default About;
+    IonContent, 
+    IonHeader, 
+    IonMenuButton, 
+    IonPage, 
+    IonTitle, 
+    IonToolbar,
+    IonButton,  
+    IonPopover
+} from '@ionic/react';
+
+const About: React.FC = () => {
+  return (
+    <IonPage>
+      {/* Header Section */}
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>About</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      {/* Content Section */}
+      <IonContent fullscreen className="ion-padding">
+        {/* Click Popover */}
+        <IonButton id="click-trigger">About me</IonButton>
+        <IonPopover trigger="click-trigger" triggerAction="click">
+          <IonContent className="ion-padding">HI I AM ME!</IonContent>
+        </IonPopover>
+
+        {/* Right-Click Popover */}
+        <IonButton id="context-menu-trigger">About this website</IonButton>
+        <IonPopover trigger="context-menu-trigger" triggerAction="context-menu">
+          <IonContent className="ion-padding">THIS WEBSITE IS ABOUT ADVENTURE!</IonContent>
+        </IonPopover>
+
+        {/* Hover Popover */}
+        <IonButton id="hover-trigger">Terms and Condition</IonButton>
+        <IonPopover trigger="hover-trigger" triggerAction="hover">
+          <IonContent className="ion-padding">AGREE!</IonContent>
+        </IonPopover>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default About;
