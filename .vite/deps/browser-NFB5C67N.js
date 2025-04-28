ae1d2f44c149ca59eafd75ff89a703286c5b9d26
NHEL360 <nhelmeynard@gmail.com>
2025-04-28 13:02:29 +0800

    added new design on log in and register

---
+++ .vite/deps/browser-NFB5C67N.js
@@ -0,0 +1,18 @@
+import {
+  Headers,
+  Request,
+  Response,
+  browser_default,
+  fetch,
+  init_browser
+} from "./chunk-MMDX567K.js";
+import "./chunk-YTB4TACA.js";
+init_browser();
+export {
+  Headers,
+  Request,
+  Response,
+  browser_default as default,
+  fetch
+};
+//# sourceMappingURL=browser-NFB5C67N.js.map
